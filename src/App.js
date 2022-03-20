db2ea011f07f00c9b82e14c1304f69806a6ead19
harshkc <harsh.hkc1006@gmail.com>
2022-03-20 10:26:50 +0530

    adds navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <>
+      <Header />
+      <div></div>
+    </>
+  );
 }
 
 export default App;
